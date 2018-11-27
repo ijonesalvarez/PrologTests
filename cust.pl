d75d1a9378405dee5bb0674d6a88aee48a1a96c6
ijonesalvarez <email_id>
2018-11-26 21:06:43 -0800

    more tests

---
+++ cust.pl
@@ -0,0 +1,10 @@
+customer(jones, imperial, aaa).
+customer(lopez, 'el centro', aaa).
+
+sale(111, apple, 5).
+sale(222, banana, 10).
+sale(333, orange, 7).
+
+inventory(111, 10).
+inventory(222, 9).
+inventory(333, 15).
---
+++ gene.pl
@@ -0,0 +1,15 @@
+male(mark).
+male(israel).
+male(diego).
+male(manuel).
+male(lilo).
+
+female(maria).
+female(clarina).
+
+parent(mark, israel).
+parent(mark, diego).
+
+parent(manuel, maria).
+parent(manuel, lilo).
+
